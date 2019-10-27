ffe649da2ee310fdd9263accf1ad7f31eeca7288
KazukiMatsu <eizan.denta@gmail.com>
2019-10-27 16:36:31 +0900

    user_profielsモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191027070707) do
+ActiveRecord::Schema.define(version: 20191027072255) do
+
+  create_table "user_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "family_name",                    null: false
+    t.string   "last_name",                      null: false
+    t.string   "family_name_kana",               null: false
+    t.string   "last_name_kana",                 null: false
+    t.text     "introduction",     limit: 65535
+    t.integer  "birth_year",                     null: false
+    t.integer  "birth_month",                    null: false
+    t.integer  "birth_day",                      null: false
+    t.text     "icon_image",       limit: 65535
+    t.integer  "total_sales"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "phone",                               null: false
@@ -25,4 +42,5 @@ ActiveRecord::Schema.define(version: 20191027070707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "user_profiles", "users"
 end
