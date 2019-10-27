7376b7fa4cdaa328afe791ae420c91f7cad8b24a
sanpeiyuya <kingdommakao@gmail.com>
2019-10-27 17:04:43 +0900

    ユニコーンの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
