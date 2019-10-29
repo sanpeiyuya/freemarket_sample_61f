7c2527b233c65b4c8be663b97473504b9ca76ba0
KazukiMatsu <eizan.denta@gmail.com>
2019-10-29 15:19:29 +0900

    categoryモデル、categoriesテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029044709) do
+ActiveRecord::Schema.define(version: 20191029061655) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20191029044709) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.integer  "size_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["size_id"], name: "index_categories_on_size_id", using: :btree
   end
 
   create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -69,5 +77,6 @@ ActiveRecord::Schema.define(version: 20191029044709) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "categories", "sizes"
   add_foreign_key "user_profiles", "users"
 end
