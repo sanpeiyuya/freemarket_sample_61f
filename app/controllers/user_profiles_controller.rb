b21e38c6b181648f1d54c664ae8b48b1fa037e8d
sanpeiyuya <kingdommakao@gmail.com>
2019-10-31 15:45:52 +0900

    view

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -3,5 +3,12 @@ class UserProfilesController < ApplicationController
     @profile = UserProfile.find(1)
   end
   def update
+    binding.pry
+    UserProfile.find(params[:id]).update(introduction_params)
+  end
+
+  private
+  def introduction_params
+    params.require(:uesr_profile).permit(:introduction)
   end
 end
