4b8b413a40f57a0bd2a0360174f4def2aaad1ef3
sanpeiyuya <kingdommakao@gmail.com>
2019-11-01 20:54:53 +0900

    コントローラー記述ミスの修正

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,14 +1,2 @@
 class UserProfilesController < ApplicationController
-  
-  def edit
-    @profile = UserProfile.find(1)
-  end
-  def update
-    UserProfile.find(params[:id]).update(introduction_params)
-  end
-
-  private
-  def introduction_params
-    params.require(:user_profile).permit(:nickname, :introduction)
-  end
 end
