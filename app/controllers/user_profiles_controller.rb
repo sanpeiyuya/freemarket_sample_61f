3d07bb82f3d93e3f1dd65f476baeb766c04236c0
sanpeiyuya <kingdommakao@gmail.com>
2019-11-02 17:18:36 +0900

    本人登録途中

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,2 +1,15 @@
 class UserProfilesController < ApplicationController
+  def edit
+    @profile = UserProfile.find(current_user.id)
+  end
+
+  def update
+    UserProfile.find(params[:id]).update(introduction_params)
+  end
+
+  private
+  def introduction_params
+    params.require(:user_profile).permit(:nickname, :introduction)
+  end
+
 end
