826c75f51e5e355cb743bf35f0d10aed2f78b5af
KazukiMatsu <eizan.denta@gmail.com>
2019-11-02 14:15:12 +0900

    ブランド検索機能を実装

--- app/assets/javascripts/exhibit_brand.js
+++ app/assets/javascripts/exhibit_brand.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  // リスト作成
   function creatList(brands) {
     var html = ``;
     brands.forEach(function(brand) {
@@ -7,14 +8,14 @@ $(function() {
     })
     return html;
   }
-
+  // ブランド検索機能
   $('#display_item_brand_id').on('keyup', function() {
     // リストを空にする
     $('.brand_search_result__box').children().remove();
+    // キーワード取得
     var keyword = $('#display_item_brand_id').val();
     // キーワードが空の時は何もしない
     if ( keyword != "" ) {
-      console.log("通った");
       $.ajax({
         url: '/display_items/brand_search',
         type: 'post',
@@ -31,5 +32,15 @@ $(function() {
         alert("ブランドの取得に失敗しました");
       })
     }
+  });
+
+  // ブランド選択機能
+  $(document).on('click', ".brand_search_result__box__content", function() {
+    // クリックされた要素の名前を取得
+    var text = this.innerText;
+    // 名前をinputに挿入
+    $("#display_item_brand_id").val(text);
+    // 検索リスト削除
+    $('.brand_search_result__box').children().remove();
   });
 });
