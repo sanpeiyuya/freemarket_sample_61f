5ed4acc1f278d497f665fc19e498745c7ed2ce41
sanpeiyuya <kingdommakao@gmail.com>
2019-11-03 21:36:45 +0900

    userとAddreddにアソシエーション記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one :address, dependent: :destroy
+  has_one :user_profile, dependent: :destroy
   has_many :display_items, dependent: :delete_all
 end
