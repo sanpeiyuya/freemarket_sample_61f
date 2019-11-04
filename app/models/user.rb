dc5d0afe23156ac39358e8d4e514256bb6d08ee9
KazukiMatsu <eizan.denta@gmail.com>
2019-11-04 17:34:57 +0900

    commnetsテーブルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :display_items, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
 end
