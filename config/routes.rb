78ef0298cf1d22d53b20fd561fbd9a4b7ce122ac
kevinseishoc <caduceus.c@keio.jp>
2019-11-06 18:15:23 +0900

    login&logout1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy" 
+  end
 
   root 'items#index'
   # ユーザー新規作成系のルーティング(※暫定！)
