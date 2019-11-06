037cb518a8a930645603e0f8f2d4b854d1fdc04a
kevinseishoc <caduceus.c@keio.jp>
2019-11-07 02:42:10 +0900

    To-fix-userprofiles-table

--- db/migrate/20191027072255_create_user_profiles.rb
+++ db/migrate/20191027072255_create_user_profiles.rb
@@ -3,9 +3,9 @@ class CreateUserProfiles < ActiveRecord::Migration[5.0]
     create_table :user_profiles do |t|
       t.references  :user,                   foreign_key: true
       t.string      :family_name,            null: false
-      t.string      :last_name,              null: false
+      t.string      :given_name,              null: false
       t.string      :family_name_kana,       null: false
-      t.string      :last_name_kana,         null: false
+      t.string      :given_name_kana,         null: false
       t.text        :introduction
       t.integer     :birth_year,             null: false
       t.integer     :birth_month,            null: false
