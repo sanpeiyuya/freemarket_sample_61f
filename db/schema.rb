eaaac0899592d0b6801744f4bbe4bd23997472e2
KazukiMatsu <eizan.denta@gmail.com>
2019-11-09 17:52:41 +0900

    UseProfileをUserテーブルにマージ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191107041510) do
+ActiveRecord::Schema.define(version: 20191109084531) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -92,33 +92,26 @@ ActiveRecord::Schema.define(version: 20191107041510) do
     t.index ["ancestry"], name: "index_sizes_on_ancestry", using: :btree
   end
 
-  create_table "user_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.string   "family_name",                    null: false
-    t.string   "given_name",                     null: false
-    t.string   "family_name_kana",               null: false
-    t.string   "given_name_kana",                null: false
-    t.text     "introduction",     limit: 65535
-    t.integer  "birth_year",                     null: false
-    t.integer  "birth_month",                    null: false
-    t.integer  "birth_day",                      null: false
-    t.text     "icon_image",       limit: 65535
-    t.integer  "total_sales"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "nickname"
-    t.index ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "phone",                               null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "phone",                                             null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "family_name"
+    t.string   "given_name"
+    t.string   "family_name_kana"
+    t.string   "given_name_kana"
+    t.integer  "birth_year"
+    t.integer  "birth_month"
+    t.integer  "birth_day"
+    t.string   "nickname"
+    t.text     "introduction",           limit: 65535
+    t.text     "icon_image",             limit: 65535
+    t.integer  "total_sales"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -132,5 +125,4 @@ ActiveRecord::Schema.define(version: 20191107041510) do
   add_foreign_key "display_items", "sizes"
   add_foreign_key "display_items", "users"
   add_foreign_key "images", "display_items"
-  add_foreign_key "user_profiles", "users"
 end
