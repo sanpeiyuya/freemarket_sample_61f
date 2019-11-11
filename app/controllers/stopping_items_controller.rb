fe8d3e1cfc8b9b0cf95b2e6de6874310abae9a0e
KazukiMatsu <eizan.denta@gmail.com>
2019-11-11 12:33:31 +0900

    フラッシュ機能を実装

--- app/controllers/stopping_items_controller.rb
+++ app/controllers/stopping_items_controller.rb
@@ -3,16 +3,18 @@ class StoppingItemsController < ApplicationController
   def create
     @stopping_item = StoppingItem.new(display_item_id: params[:format])
     if @stopping_item.save
-      redirect_to display_items_mypages_path
+      redirect_to display_item_path(params[:format]), notice: '出品を停止しました'
     else
+      redirect_to display_item_path(params[:format]), alert: '出品の停止に失敗しました'
     end
   end
 
   def destroy
     @stopping_item = StoppingItem.find_by(display_item_id: params[:id])
     if @stopping_item.destroy
-      redirect_to display_items_mypages_path
+      redirect_to display_item_path(params[:id]), notice: '出品を再開しました'
     else
+      redirect_to display_item_path(params[:id]), alert: '出品の再開に失敗しました'
     end
   end
 
