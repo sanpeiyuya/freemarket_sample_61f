3824499d10bd2978e74517069f88bf725ba4ea72
KazukiMatsu <eizan.denta@gmail.com>
2019-11-13 14:19:31 +0900

    updateに失敗した時の戻り先を設定

--- app/controllers/display_items_controller.rb
+++ app/controllers/display_items_controller.rb
@@ -58,6 +58,14 @@ class DisplayItemsController < ApplicationController
     @display_item = DisplayItem.find(params[:id])
     if @display_item.update(display_item_params)
       redirect_to root_path
+    else
+      @images = @display_item.images
+      # セレクトボックスの中身を取得
+      get_select_box_contents
+      # edit用の初期情報を取得
+      get_edit_variables  
+      # editページに戻す
+      render action: :edit
     end
   end
 
