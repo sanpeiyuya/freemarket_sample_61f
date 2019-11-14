7e356d0a552a3128902a21f9fded8dd4671623de
KazukiMatsu <eizan.denta@gmail.com>
2019-11-14 14:56:06 +0900

    selectメソッドに変更し、ビューへの渡し方も修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -5,22 +5,12 @@ class MypagesController < ApplicationController
 
   def display_items
     @items = current_user.display_items
-    @display_items = []
-    @items.each do |item|
-      if item.trading_item.nil? && item.finished_item.nil? 
-        @display_items << item
-      end
-    end
+    @display_items = @items.select { |item| item.trading_item.nil? && item.finished_item.nil? }
   end
 
   def trading_items
     @items = current_user.display_items
-    @trading_items = []
-    @items.each do |item|
-      if item.trading_item && item.finished_item.nil? 
-        @trading_items << item.trading_item
-      end
-    end
+    @has_trading_items = @items.select { |item| item.trading_item && item.finished_item.nil? }
   end
 
   def finished_items
