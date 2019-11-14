95f6f80a2ef6f408d733cb916591c70676807d14
KazukiMatsu <eizan.denta@gmail.com>
2019-11-14 12:54:40 +0900

    コメント削除

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,8 @@
 class MypagesController < ApplicationController
   def index
-    # @profile = UserProfile.find_by(user_id: current_user.id)
     @display_item = DisplayItem.new
   end
-  # 商品削除機能（このコメントは後で消す）
+
   def display_items
     @items = current_user.display_items
     @display_items = []
@@ -13,7 +12,7 @@ class MypagesController < ApplicationController
       end
     end
   end
-  # 商品削除機能（このコメントは後で消す）
+
   def trading_items
   end
 
