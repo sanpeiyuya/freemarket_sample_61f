ed57b1d6a464556313e874e166ff21d5541e081c
KazukiMatsu <eizan.denta@gmail.com>
2019-11-18 14:52:32 +0900

    display_itemのテスト修正

--- spec/models/display_item_spec.rb
+++ spec/models/display_item_spec.rb
@@ -2,21 +2,60 @@ require 'rails_helper'
 
 describe DisplayItem do
   describe '#create' do
-    it "「name」抜きでは商品が作れない" do
-      category = create(:category)
+    it "「name」なしで商品は作れない" do
+      size = create(:size)
+      category = create(:category, size_id: size.id)
       # カテゴリが必要なので作る
       display_item = build(:display_item, category_id: category.id, name: "")
       display_item.valid?
       expect(display_item.errors[:name]).to include("を入力してください")
     end
+
+    it "「name」が41文字で商品が作れない" do
+      size = create(:size)
+      category = create(:category, size_id: size.id)
+      # カテゴリが必要なので作る
+      display_item = build(:display_item, category_id: category.id, name: "あいうえおかきくけこあいうえおかきくけこあいうえおかきくけこあいうえおかきくけこを")
+      display_item.valid?
+      expect(display_item.errors[:name]).to include("は40文字以内で入力してください")
+    end
+
+    it "「description」なしで商品は作れない" do
+      size = create(:size)
+      category = create(:category, size_id: size.id)
+      # カテゴリが必要なので作る
+      display_item = build(:display_item, category_id: category.id, description: "")
+      display_item.valid?
+      expect(display_item.errors[:description]).to include("を入力してください")
+    end
+
+    it "「description」が1001文字で商品が作れない" do
+      size = create(:size)
+      category = create(:category, size_id: size.id)
+      # カテゴリが必要なので作る
+      display_item = build(:display_item, category_id: category.id, description: "あああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいあああああいいいいいう")
+      display_item.valid?
+      expect(display_item.errors[:description]).to include("は1000文字以内で入力してください")
+    end
+
+    it "「category_id」なしで商品は作れない" do
+      size = create(:size)
+      category = create(:category, size_id: size.id)
+      # カテゴリが必要なので作る
+      display_item = build(:display_item, category_id: "")
+      display_item.valid?
+      expect(display_item.errors[:category_id]).to include("を入力してください")
+    end
+
   end
 
   describe '#destroy' do
-    it 'redirect_to_302' do
+    it 'display_itemのレコードを削除できるか' do
       user = create(:user)
-      category = create(:category)
+      size = create(:size)
+      category = create(:category, size_id: size.id)
       # カテゴリとユーザーが必要なので作る
-      display_item = create(:display_item, category_id: category.id, user_id: user.id, brand_id: "", size_id: "")
+      display_item = create(:display_item, category_id: category.id, size_id: size.id, user_id: user.id, brand_id: "")
       expect{ display_item.destroy }.to change{ DisplayItem.count }.by(-1)
     end
   end
