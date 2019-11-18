6cdc0a3ec47af64140333797816a97f0c766b866
KazukiMatsu <eizan.denta@gmail.com>
2019-11-18 13:51:17 +0900

    display_itemのcontrollerテストの内容をモデルに移したのでこっちは削除

--- spec/controllers/display_items_controller_spec.rb
+++ spec/controllers/display_items_controller_spec.rb
@@ -1,17 +1,1 @@
 require 'rails_helper'
-
-describe DisplayItemsController do
-
-  describe 'Delete#destroy' do
-    it 'redirect_to_302' do
-      display_item = create(:display_item)
-      delete :destroy, params: {id: display_item}
-      expect(response.status).to eq 302
-    end
-    it 'delete_display_item' do
-      display_item = create(:display_item)
-      expect{delete :destroy, params: {id: display_item}}.to change(DisplayItem, :count).by(-1)
-    end
-  end
-
-end
